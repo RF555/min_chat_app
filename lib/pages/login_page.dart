d3c681ab98a16c6306b0d73d6e231d664ceb2534
Roey Feingold <roey.feingold@gmail.com>
2025-02-13 15:58:02 +0200

    Add reusable MyTextField component and update login page UI

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,12 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:min_chat_app/components/my_textfeild.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // logo
+            Icon(
+            Icons.message,
+            size: 60,
+            color: Theme.of(context).colorScheme.primary,
+            ),
+            const SizedBox(height: 50),
+            // welcome back message
+            Text(
+              'Welcome back!',
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(height: 50),
+
+            // email textfield
+            MyTextField(
+              hintText: "Email",
+              obscureText: false,
+              controller: _emailController,
+            ),
+            const SizedBox(height: 10),
+
+            // password textfield
+            MyTextField(
+              hintText: "Password",
+              obscureText: true,
+              controller: _passwordController,
+            ),
+            const SizedBox(height: 25),
+
+            // login button
+
+            // register button
+          ],
+        )
+      )
     );
   }
 }
